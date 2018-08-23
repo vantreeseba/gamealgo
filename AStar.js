95a0fb6f8a3addf1c7fddfdf9a8aa8d57c710e09
Benjamin Van Treese <vantreeseba@gmail.com>
2018-08-23 11:14:16 -0500

    Cleanup astar, add tests

--- AStar.js
+++ AStar.js
@@ -1,109 +1,140 @@
 const Heap = require('game-ds').Heap;
 
 /**
- * Find the shortest path from one node to another in a graph.
- *
- * @param {} start
- * @param {} goal
+ * A* Search algorithm implementation.
  */
-function AStar(graph, start, goal) {
+class AStar {
+  /**
+   * Reset astar runner so you can use a diff graph, start or goal.
+   *
+   * @param {} graph
+   * @param {} start
+   * @param {} goal
+   */
+  reset(graph, start, goal) {
+    this.graph = graph;
+    this.start = start;
+    this.goal = goal;
+
+    // The set of nodes already evaluated
+    this.closedSet = new Set();
+
+    // For each node, which node it can most efficiently be reached from.
+    // If a node can be reached from many nodes, cameFrom will eventually contain the
+    // most efficient previous step.
+    // cameFrom := an empty map
+    this.cameFrom = new Map();
+
+    // For each node, the cost of getting from the start node to that node.
+    // map with default value of Infinity
+    this.gScore = new Map();
+
+    // The cost of going from start to start is zero.
+    this.gScore.set(start, 0);
+
+    // For each node, the total cost of getting from the start node to the goal
+    // by passing by that node. That value is partly known, partly heuristic.
+    // fScore := map with default value of Infinity
+    this.fScore = new Map();
+
+    // // For the first node, that value is completely heuristic.
+    this.fScore.set(start, this.heuristicCostEstimate(start, goal));
+
+    // The set of currently discovered nodes that are not evaluated yet.
+    // Initially, only the start node is known.
+    this.openSet = new Heap((a, b) => this.fScore.get(a) < this.fScore.get(b));
+    this.openSet.push(start);
+  }
   /**
    * Rebuild path from.
    *
-   * @param {} cameFrom
    * @param {} current
    */
-  function reconstructPath(cameFrom, current) {
+  reconstructPath(current) {
     const path = [];
 
     while(current) {
       path.unshift(current);
-      current = cameFrom.get(current);
+      current = this.cameFrom.get(current);
     }
 
     return path;
   }
-
   /**
    * Calculate the heuristic cost.
    *
    * @param {} start
    * @param {} goal
    */
-  function heuristicCostEstimate(start, goal) {
+  heuristicCostEstimate(node, goal) {
     return 0;
   }
-
   /**
    * Calculate distance between two nodes
    *
    * @param {*} v1
    * @param {*} v2
    */
-  function distBetween(v1, v2) {
-    const dist = graph.adjList.get(v1).get(v2);
+  distBetween(v1, v2) {
+    const dist = this.graph.adjList.get(v1).get(v2);
     return dist !== undefined ? dist : Infinity;
   }
 
-  // The set of nodes already evaluated
-  const closedSet = new Set();
-
-  // For each node, which node it can most efficiently be reached from.
-  // If a node can be reached from many nodes, cameFrom will eventually contain the
-  // most efficient previous step.
-  // cameFrom := an empty map
-  const cameFrom = new Map();
-
-  // For each node, the cost of getting from the start node to that node.
-  // map with default value of Infinity
-  const gScore = new Map();
-
-  // The cost of going from start to start is zero.
-  gScore.set(start, 0);
-
-  // For each node, the total cost of getting from the start node to the goal
-  // by passing by that node. That value is partly known, partly heuristic.
-  // fScore := map with default value of Infinity
-  const fScore = new Map();
-
-  // // For the first node, that value is completely heuristic.
-  fScore.set(start, heuristicCostEstimate(start, goal));
-
-  // The set of currently discovered nodes that are not evaluated yet.
-  // Initially, only the start node is known.
-  const openSet = new Heap((a, b) => fScore.get(a) < fScore.get(b));
-  openSet.push(start);
-
-
-  // while openSet is not empty
-  while(openSet.elements.length > 0) {
+  /**
+   * Take a single step through the algorithm, this is mostly exposed for doing demos/visualization.
+   *
+   * @return {Boolean|Array} A bool indicating it's still running, or an array of the shortest path.
+   */
+  step(){
+    if(this.openSet.elements.length === 0) {
+      return false;
+    }
     // the node in openSet having the lowest fScore[] value
-    const current = openSet.pop();
-    if(current === goal){
-      return reconstructPath(cameFrom, current);
+    const current = this.openSet.pop();
+    if(current === this.goal){
+      return this.reconstructPath(current);
     }
 
-    closedSet.add(current);
+    this.closedSet.add(current);
 
-    graph.getNeighbors(current).forEach(neighbor => {
-      if(closedSet.has(neighbor)) {
+    this.graph.getNeighbors(current).forEach(neighbor => {
+      if(this.closedSet.has(neighbor)) {
         return;
       }
 
-
-      let tempGScore = gScore.get(current) + distBetween(current, neighbor);
-      if(!gScore.has(neighbor)) {
-        gScore.set(neighbor, Infinity);
+      let tempGScore = this.gScore.get(current) + this.distBetween(current, neighbor);
+      if(!this.gScore.has(neighbor)) {
+        this.gScore.set(neighbor, Infinity);
       }
-      if(tempGScore >= gScore.get(neighbor)) {
+      if(tempGScore >= this.gScore.get(neighbor)) {
         return;
       }
 
-      cameFrom.set(neighbor, current);
-      gScore.set(neighbor, tempGScore);
-      fScore.set(neighbor, tempGScore + heuristicCostEstimate(neighbor, goal));
-      openSet.push(neighbor);
+      this.cameFrom.set(neighbor, current);
+      this.gScore.set(neighbor, tempGScore);
+      this.fScore.set(neighbor, tempGScore + this.heuristicCostEstimate(neighbor, this.goal));
+      this.openSet.push(neighbor);
     });
+
+    // Return true to say it's still running.
+    return true;
+  }
+
+  /**
+   * Find the shortest path from one node to another in a graph.
+   *
+   * @param {} graph
+   * @param {} start
+   * @param {} goal
+   */
+  run(graph, start, goal) {
+    this.reset(graph, start, goal);
+    let running = true;
+    while(running === true){
+      running = this.step();
+    }
+
+    return running;
   }
 }
 
--- test/AStar.js
+++ test/AStar.js
@@ -3,32 +3,53 @@ var AStar = require('../AStar');
 var Graph = require('game-ds/Graph');
 
 let graph;
+let astar;
 
 const test = {
   AStar: {
     beforeEach: () => {
       graph = new Graph();
+      astar = new AStar();
     },
     constructor: {
       'should construct a graph': () => {
         assert.isOk(graph);
       },
     },
-    'should find the shortest path': () => {
-      graph.addVertex('A');
-      graph.addVertex('B');
-      graph.addVertex('C');
-      graph.addVertex('D');
+    run: {
+      'should find the shortest path': () => {
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
 
-      graph.addEdge('A', 'B', 1);
-      graph.addEdge('A', 'C', 0);
-      graph.addEdge('B', 'D', 1);
-      graph.addEdge('C', 'D', 1);
+        graph.addEdge('A', 'B', 1);
+        graph.addEdge('A', 'C', 0);
+        graph.addEdge('B', 'D', 1);
+        graph.addEdge('C', 'D', 1);
 
-      // Path should be A C D.
+        // Path should be A C D.
+        const expected = ['A', 'C', 'D'];
 
-      const path = AStar(graph, 'A', 'D');
-      console.log(path);
+        const path = astar.run(graph, 'A', 'D');
+        assert.deepEqual(expected, path);
+      },
+      'should return false when no path is found': () => {
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+
+        graph.addEdge('A', 'B', 1);
+        graph.addEdge('C', 'D', 1);
+
+        // Path should be A C D.
+        const expected = false;
+
+        const path = astar.run(graph, 'A', 'D');
+        assert.deepEqual(expected, path);
+
+      }
     }
   }
 };
